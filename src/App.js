aafbce0e9ce6b5b07c79bfd5ea66fbaf3ec8391c
Daniel Rendón <drendonr3@gmail.com>
2021-04-19 10:41:15 -0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
-import Header from './componentes/Header'
-import Inicio from './componentes/Inicio'
-import Blog from './componentes/Inicio'
-import AcercaDe from './componentes/AcercaDe'
+import styled from 'styled-components';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Header from './componentes/Header';
+import Inicio from './componentes/Inicio';
+import Blog from './componentes/Blog';
+import AcercaDe from './componentes/AcercaDe';
+import Post from './componentes/Post';
+import Error404 from './componentes/Error404';
 
 const App = () => {
   return ( 
-    <BrowserRouter>
-      <div>
+    <BrowserRouter> 
+      <ContenedorPrincipal>
         <Header></Header>
 
-        <main>
-          <Route path="/" exact={true} component={Inicio}/>
-          <Route path="/blog" exact={true} component={Blog}/>
-          <Route path="/acercade" exact={true} component={AcercaDe}/>
-        </main>
-      </div>
+        <Main>
+          <Switch>
+            <Route path="/" exact={true} component={Inicio}/>
+            <Route path="/blog" exact={true} component={Blog}/>
+            <Route path="/acercade" exact={true} component={AcercaDe}/>
+            <Route path="/post/:id" exact={true} component={Post}/>
+            <Route component={Error404}></Route>
+          </Switch>
+        </Main>
+      </ContenedorPrincipal>
     </BrowserRouter>
-   );
-}
+   );}
  
+const ContenedorPrincipal= styled.div`
+  padding:40px;
+  width:90%;
+  max-width:700px;
+`;
+
+const Main= styled.main`
+  background:#fff;
+  padding:40px;
+  border-radius:10px;
+  box-shadow: 0px 0px 5px rgba(129,129,129,0.1);
+`;
 export default App;
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,16 +1,46 @@
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
     return ( 
-        <header>
-          <h1>Hola Mundo</h1>
-          <nav>
-            <Link to="/google.com" >Inicio</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/acercade">Acerca De</Link>
-          </nav>
-        </header>
+        <ContenedorHeader>
+          <Titulo>Mi Blog Personal</Titulo>
+          <Menu>
+            <NavLink to="/" exact={true}>Inicio</NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/acercade">Acerca De</NavLink>
+          </Menu>
+        </ContenedorHeader>
      );
 }
- 
+
+const ContenedorHeader = styled.header`
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+const Titulo = styled.h1`
+  text-transform:uppercase;
+  margin-bottom: 10px;
+  font-size:26px;
+`;
+
+const Menu = styled.nav`
+  a {
+    text-decoration: none;
+    color:#165168;
+    margin:0 10px;
+  }
+
+  a:hover{
+    color:#191668;
+  }
+
+  a.active {
+    padding-bottom:3px;
+    border-bottom: 2px solid #165158;
+    
+  }
+`;
+
 export default Header;
